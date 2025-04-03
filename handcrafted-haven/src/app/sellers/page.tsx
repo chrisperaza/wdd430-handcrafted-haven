ca3395f453421c3db1e78cbb5a1d19660c12ae52
Dereckr <dereckromos@gmail.com>
2025-04-03 15:27:15 -0400

    update

--- handcrafted-haven/src/app/sellers/page.tsx
+++ handcrafted-haven/src/app/sellers/page.tsx
@@ -1,3 +1,8 @@
+import { productsArray } from '@/database/products';
+import { sellersArray } from '@/database/sellers';
+import ProductCard from '@/app/ui/products/card';
+
+
 export default function Page(){
     return 
 }
