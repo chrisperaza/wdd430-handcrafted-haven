33cf9536b0cf0ade5decf95b253bb4898f5a142c
Dereckr <dereckromos@gmail.com>
2025-04-07 21:17:09 -0400

    update

--- handcrafted-haven/src/app/sellers/[id]/page.tsx
+++ handcrafted-haven/src/app/sellers/[id]/page.tsx
@@ -1,20 +1,21 @@
 import { productsArray } from '@/database/products';
 import { sellersArray } from '@/database/sellers';
-import ProductCard from '@/app/ui/products/card';
+import ProductCard from '@/app/ui/sellers/card';
+import Image from 'next/image';
 
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const id = parseInt(params.id);
   
-    const product = productsArray.find((product) => product.id === id);
-    console.log(product);
-    if (!product) {
+    const seller = sellersArray.find((seller) => seller.id === id);
+    console.log(seller);
+    if (!seller) {
       return <div>Product not found</div>;
     }
   
-    const seller = sellersArray.find(
-      (seller) => seller.username === product.sellerUsername
+    const products = productsArray.filter(
+      (product) => product.sellerUsername === seller.username
     );
     console.log(seller);
     if (!seller) {
@@ -29,18 +30,25 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
             <h2 className='text-3xl font-bold text-gray-900 sm:text-4xl'>
               {`Seller's Page | ${seller.fullname}`}
             </h2>
-            <p className='text-gray-600 mt-2 text-lg'>By {seller.fullname}</p>
+            <p className='text-gray-600 mt-2 text-lg flex items-center gap-2'><Image alt='Seller'
+              src={seller.image}
+              width={120}
+              height={120}
+              className=' rounded-full ring-2 ring-white'/> By {seller.fullname}</p>
           </div>
+          
+          
   
           {/* Content Grid */}
           <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 items-start'>
             {/* Product Card Section with Zoom Effect */}
+            {products.map((product)=>(
             <div className='flex justify-center'>
               <div className='overflow-hidden rounded-xl shadow-md transform transition duration-300 ease-in-out hover:scale-105'>
                 <ProductCard product={product} seller={seller} />
               </div>
             </div>
-  
+            ))}
           </div>
         </div>
       </>
---
+++ handcrafted-haven/src/app/ui/sellers/card.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+export default function ProductCardSeller({
+  product,
+  seller,
+}: {
+  product: {
+    id: number;
+    productName: string;
+    description: string;
+    price: number;
+    image: string;
+    sellerUsername: string;
+  };
+  seller: {
+    id: number;
+    fullname: string;
+    username: string;
+    image: string;
+  };
+}) {
+  return (
+    <>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <Image
+          className='w-full'
+          src={product.image}
+          alt={product.productName}
+          width={0}
+          height={0}
+          sizes='100vw'
+          style={{ width: '100%', height: 'auto' }}
+        />
+        <div className='px-6 py-4'>
+          <div className='font-bold text-xl mb-2'>{product.productName}</div>
+          <p className='text-gray-700 text-base'>${product.price}</p>
+          <p className='text-gray-700 text-base'>{product.description}</p>
+        </div>
+        <div className='px-6 pt-4 pb-2'>
+          <p className='text-gray-700 text-base flex items-center gap-2'>
+            
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
