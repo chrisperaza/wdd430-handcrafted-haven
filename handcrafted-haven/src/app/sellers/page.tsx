ebd5af797dd80dc875ce9a2ba9b2342ae3244260
Dereckr <dereckromos@gmail.com>
2025-04-15 22:54:44 -0400

    update

--- handcrafted-haven/src/app/sellers/page.tsx
+++ handcrafted-haven/src/app/sellers/page.tsx
@@ -1,7 +1,12 @@
 import { getSellers } from '@/app/lib/data';
 import SearchBar from '@/app/ui/sellers/searchBar';
 import { Seller } from '@/app/ui/sellers/types';
+import { Metadata } from 'next';
 
+
+export const metadata: Metadata = {
+  title: 'Sellers',
+};
 
 export default async function Page() {
   const sellers = await getSellers();
